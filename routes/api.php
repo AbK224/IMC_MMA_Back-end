b6e2899951f3eaf98598e340ec8a590378bcd00c
unknown <koneabou669@gmail.com>
2025-09-26 17:27:28 +0000

    update et route

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/fighters',[FighterController::class,'index']);
 Route::post('/fighter',[FighterController::class,'store']);
+Route::put('/fighter/{id}',[FighterController::class,'update']);
