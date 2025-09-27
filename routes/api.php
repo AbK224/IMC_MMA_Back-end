17c9bb082f477191989b73b5d66e97a390d5e5fa
Ibrahim Ndiaye <papeibrahim.ndiaye11@gmail.com>
2025-09-27 13:00:10 +0000

    Mise en place de la methode greeting et de sa route

--- routes/api.php
+++ routes/api.php
@@ -7,5 +7,5 @@ Route::get('/fighters',[FighterController::class,'index']);
 Route::post('/fighter',[FighterController::class,'store']);
 Route::delete('/fighter/{id}',[FighterController::class,'destroy']);
 Route::get('/fighters/report/summary',[FighterController::class,'summary']);
-
+Route::get('/fighters/{id}/greeting',[FighterController::class,'greeting']);
 
