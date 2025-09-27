5d7a41319e9a93764edd2043b6042aa82c30f16c
Ibrahim Ndiaye <papeibrahim.ndiaye11@gmail.com>
2025-09-27 15:19:06 +0000

    Ajout des suggestions aux codes

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ Route::get('/fighters',[FighterController::class,'index']);
 Route::post('/fighter',[FighterController::class,'store']);
 Route::delete('/fighter/{id}',[FighterController::class,'destroy']);
 Route::get('/fighters/report/summary',[FighterController::class,'summary']);
-Route::get('/fighters/{id}/greeting',[FighterController::class,'greeting']);
+Route::get('/fighter/greeting/{id}',[FighterController::class,'greeting']);
 Route::put('/fighter/{id}',[FighterController::class,'update']);
 
 
